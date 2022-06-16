996efdbf0493a5d74e07e766b7d427ff3165f202
Tu Nombre <you@example.com>
2022-06-16 03:37:44 +0200

    Esto ya esta, solo falta lo del slug

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -30,7 +30,7 @@ file.close()
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', '')) != False
 
-ALLOWED_HOSTS = ['','127.0.0.1']
+ALLOWED_HOSTS = ['infinite-hamlet-92643.herokuapp.com','127.0.0.1']
 
 
 # Application definition
